d9576fd0c557a6a00df06000f2413f52af22f84a
Hugo <hugomaneira@gmail.com>
2019-05-14 13:00:17 +0100

    stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
+import Favicon from 'react-favicon';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        celebrities:[]
+      };
+    }
+    hoverOn() {
+      console.log('hoverOn')
+      this.setState({
+          hoverOn:true
+      })
+  }
+    hoverOff() {
+      console.log('hoverOff')
+      this.setState({
+          hoverOn: false
+      })
+  }
+
+    render() {
+      return (
+        
+        // <div className="All">
+        <div className="App">
+        <h1>Movie Celebrities</h1>
+      
+      {this.state.celebrities.map(celebrities => 
+      <ul key={celebrities.id}><img src={"https://image.tmdb.org/t/p/w185"
+      +celebrities.profile_path} alt="profile"/>
+      {celebrities.name}
+      </ul>)}
+        
+        
+        {/* if (!this.hover.hoverOn) { }
+        return <img onClick={()=>this.turnOn()}  />
+    }
+    else {
+        return <img onClick={()=>this.turnOff()} />
+} */}
+      </div>
+    // </div>
+    )
+  }
+
+
+
+  componentDidMount() {
+    axios.get('https://api.themoviedb.org/3/person/popular?page=5&api_key=d7d00236f3a0891b3c07fd98f6f189cd')
+      .then((res) => { 
+        // console.log(res.data.results)      
+        this.setState({celebrities:res.data.results});
+      })
+    }
 }
 
-export default App;
+ReactDOM.render(
+  <div>
+    <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico" />
+  </div>
+, document.getElementById('root'));
